20c0e439f95b00825125e396f28ccca8b657aa36
Amir Diba <151319977+Amir8-5@users.noreply.github.com>
2025-06-28 17:18:30 -0400

    added a friends bar, when you click a friend, it goes to them

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from core.models import Friend, Place
+from core.serializer import FriendSerializer, PlaceSerializer
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    friends = Friend.objects.all()
+    context = {
+        "friends": friends
+    }
+    return render(request, "home.html", context)
