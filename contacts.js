f40af0f0daedeb2705c98fe44259fff97aeef180
Daniel Hyman <danielHyman20@gmail.com>
2025-06-28 12:27:05 -0400

    Update

---
+++ contacts.js
@@ -0,0 +1,10 @@
+//Contact attributes
+const fullName = "", phone = "", email = "", friendEmail = "", locate = "";
+
+const contactAttributes = {
+    fullName: fullName,
+    phone: phone,
+    email: email,
+    friendEmail: friendEmail,
+    locate: locate
+}
